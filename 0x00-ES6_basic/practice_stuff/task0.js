d9f378777703453b5f4e6c7dfb884a72b5ae6c87
Agw-a <lizagwa7@gmail.com>
2023-01-16 12:27:48 +0300

    Task 0

--- 0x00-ES6_basic/practice_stuff/task0.js
+++ 0x00-ES6_basic/practice_stuff/task0.js
@@ -6,7 +6,7 @@ function getLast() {
   return ' is okay';
 }
 function taskNext() {
-  let combination = 'But sometimes let';
+  let combination = ' But sometimes let';
   combination += getLast();
 
   return combination
