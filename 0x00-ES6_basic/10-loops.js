bbe9686f6ec6eb4210721ba883ab5533ec89ba67
Agw-a <lizagwa7@gmail.com>
2023-01-16 14:10:25 +0300

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const appendRes = [];
-    for (const idx of array) {
-        appendRes.push(appendString + idx)
-    }
+  const appendRes = [];
+  for (const idx of array) {
+    appendRes.push(appendString + idx)
+  }
 
-    return appendRes;
+  return appendRes;
 }
--- 0x00-ES6_basic/practice_stuff/task1.js
+++
@@ -1,4 +0,0 @@
-function returnHowManyArguments(...argsTurn) {
-  return argsTurn.length;
-}
-console.log(returnHowManyArguments(1,2,3,3,3))
