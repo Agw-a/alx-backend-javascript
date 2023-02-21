9c75954411139f08a9d22c9c30d6a7eb27215bf1
Agw-a <lizagwa7@gmail.com>
2023-02-21 14:21:41 +0300

    Task 4

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,9 +1,19 @@
 const fs = require('fs')
 
+/**
+ * param {String} filePath -> path
+ */
 const countStudents = (filePath) => {
     if (!fs.existsSync(filePath)) {
         throw new Error('Cannot load the database')
     }
+    if (!fs.statSync(filePath).isFile()) {
+        throw new Error('Cannot load the database')
+    }
+    const readLines = fs.readFileSync(filePath, 'utf-8').toString('utf-8').trim().split('\n')
+    const stdts = {};
+    const names = readLines[0].split(',');
+    const stdNames = names.slice(0, names.length)
 }
 
 module.exports = countStudents;
---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,19 @@
+const  http = require('http');
+
+const listen_port = 1245;
+const hostname = 'localhost';
+const response_text = 'Hello Holberton School!';
+const app = http.createServer();
+
+app.on((req, response) => {
+    response.setHeader('Content-Type', 'text/plain');
+    response.setHeader('Content-Length', response_text.length)
+    response.statusCodes = 200;
+    response.write(Buffer.from(response_text))
+});
+
+app.listen(listen_port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${listen_port}/`)
+})
+
+module.exports = app;
